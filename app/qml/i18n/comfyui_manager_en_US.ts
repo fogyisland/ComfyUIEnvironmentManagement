75b4df4af68164f36bd12306e79d0258198d8ca1
fogyisland <10853913@qq.com>
2026-06-26 20:06:45 +0800

    feat(i18n): extract + translate M2 strings (~40 new entries)

--- app/qml/i18n/comfyui_manager_en_US.ts
+++ app/qml/i18n/comfyui_manager_en_US.ts
@@ -38,6 +38,39 @@
     </message>
 </context>
 <context>
+    <name>ConflictPanel</name>
+    <message>
+        <location filename="../components/ConflictPanel.qml" line="27"/>
+        <source>⚠ %1 个冲突</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/ConflictPanel.qml" line="49"/>
+        <source>类 &quot;%1&quot; 被 %2 个节点提供</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/ConflictPanel.qml" line="53"/>
+        <source>冲突: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/ConflictPanel.qml" line="59"/>
+        <source>禁用其一</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/ConflictPanel.qml" line="66"/>
+        <source>查看详情</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/ConflictPanel.qml" line="70"/>
+        <source>忽略</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>CreateEnvDialog</name>
     <message>
         <location filename="../pages/CreateEnvDialog.qml" line="10"/>
@@ -114,42 +147,52 @@
 <context>
     <name>EnvironmentDetailPanel</name>
     <message>
-        <location filename="../pages/EnvironmentDetailPanel.qml" line="37"/>
+        <location filename="../pages/EnvironmentDetailPanel.qml" line="58"/>
         <source>运行中</source>
         <translation>Running</translation>
     </message>
     <message>
-        <location filename="../pages/EnvironmentDetailPanel.qml" line="37"/>
+        <location filename="../pages/EnvironmentDetailPanel.qml" line="58"/>
         <source>已停止</source>
         <translation>Stopped</translation>
     </message>
     <message>
-        <location filename="../pages/EnvironmentDetailPanel.qml" line="42"/>
+        <location filename="../pages/EnvironmentDetailPanel.qml" line="63"/>
         <source>端口: %1</source>
         <translation>Port: %1</translation>
     </message>
     <message>
-        <location filename="../pages/EnvironmentDetailPanel.qml" line="46"/>
+        <location filename="../pages/EnvironmentDetailPanel.qml" line="67"/>
         <source>Python: %1</source>
         <translation>Python: %1</translation>
     </message>
     <message>
-        <location filename="../pages/EnvironmentDetailPanel.qml" line="58"/>
+        <location filename="../pages/EnvironmentDetailPanel.qml" line="79"/>
         <source>启动</source>
         <translation>Start</translation>
     </message>
     <message>
-        <location filename="../pages/EnvironmentDetailPanel.qml" line="65"/>
+        <location filename="../pages/EnvironmentDetailPanel.qml" line="86"/>
         <source>停止</source>
         <translation>Stop</translation>
     </message>
     <message>
-        <location filename="../pages/EnvironmentDetailPanel.qml" line="70"/>
+        <location filename="../pages/EnvironmentDetailPanel.qml" line="91"/>
         <source>删除</source>
         <translation>Delete</translation>
     </message>
     <message>
-        <location filename="../pages/EnvironmentDetailPanel.qml" line="77"/>
+        <location filename="../pages/EnvironmentDetailPanel.qml" line="104"/>
+        <source>自定义节点 (%1)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/EnvironmentDetailPanel.qml" line="112"/>
+        <source>重新扫描</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/EnvironmentDetailPanel.qml" line="138"/>
         <source>实时日志</source>
         <translation>Live Log</translation>
     </message>
@@ -197,6 +240,120 @@
     </message>
 </context>
 <context>
+    <name>NodeDetailPanel</name>
+    <message>
+        <location filename="../components/NodeDetailPanel.qml" line="40"/>
+        <source>本地信息</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/NodeDetailPanel.qml" line="45"/>
+        <source>版本:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/NodeDetailPanel.qml" line="46"/>
+        <location filename="../components/NodeDetailPanel.qml" line="48"/>
+        <location filename="../components/NodeDetailPanel.qml" line="101"/>
+        <location filename="../components/NodeDetailPanel.qml" line="103"/>
+        <source>未知</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/NodeDetailPanel.qml" line="47"/>
+        <source>作者:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/NodeDetailPanel.qml" line="49"/>
+        <source>描述:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/NodeDetailPanel.qml" line="50"/>
+        <location filename="../components/NodeDetailPanel.qml" line="53"/>
+        <location filename="../components/NodeDetailPanel.qml" line="106"/>
+        <source>无</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/NodeDetailPanel.qml" line="51"/>
+        <source>类映射:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/NodeDetailPanel.qml" line="74"/>
+        <source>远程信息 (GitHub)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/NodeDetailPanel.qml" line="78"/>
+        <source>获取中…</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/NodeDetailPanel.qml" line="78"/>
+        <source>查看远程信息</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/NodeDetailPanel.qml" line="100"/>
+        <source>Stars:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/NodeDetailPanel.qml" line="102"/>
+        <source>最后提交:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/NodeDetailPanel.qml" line="104"/>
+        <source>主页:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/NodeDetailPanel.qml" line="113"/>
+        <source>⚠ 无法获取远程信息: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/NodeDetailPanel.qml" line="123"/>
+        <source>关闭</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NodeListItem</name>
+    <message>
+        <location filename="../components/NodeListItem.qml" line="54"/>
+        <source>已禁用</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/NodeListItem.qml" line="54"/>
+        <source>启用中</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/NodeListItem.qml" line="63"/>
+        <source>启用</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../components/NodeListItem.qml" line="63"/>
+        <source>禁用</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NodeScanBusy</name>
+    <message>
+        <location filename="../components/NodeScanBusy.qml" line="24"/>
+        <source>正在扫描节点…</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>PathField</name>
     <message>
         <location filename="../components/PathField.qml" line="16"/>
@@ -224,7 +381,7 @@
     <message>
         <location filename="../pages/SettingsPage.qml" line="29"/>
         <source>修改后点击「迁移」按钮复制现有数据库到新位置</source>
-        <translation>After editing, click "Migrate" to copy the existing database to the new location</translation>
+        <translation>After editing, click &quot;Migrate&quot; to copy the existing database to the new location</translation>
     </message>
     <message>
         <location filename="../pages/SettingsPage.qml" line="41"/>
@@ -273,6 +430,26 @@
     </message>
     <message>
         <location filename="../pages/SettingsPage.qml" line="125"/>
+        <source>禁用模式</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="130"/>
+        <source>仅标记 (推荐)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="131"/>
+        <source>重命名目录 (强制跳过)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="141"/>
+        <source>folder_rename 模式下，禁用节点会把目录改名为 &lt;pkg&gt;.disabled，ComfyUI 启动时跳过</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/SettingsPage.qml" line="146"/>
         <source>默认 Python 路径</source>
         <translation>Default Python Path</translation>
     </message>
