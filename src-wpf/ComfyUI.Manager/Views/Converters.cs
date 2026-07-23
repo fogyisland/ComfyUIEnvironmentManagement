58715c76fb9ecbe4d4697f88e15ddf1c0f97191b
fogyisland <10853913@qq.com>
2026-07-23 12:56:05 +0800

    chore(wpf): drop dead BoolToRefreshTextConverter after Settings refresh button removal

--- src-wpf/ComfyUI.Manager/Views/Converters.cs
+++ src-wpf/ComfyUI.Manager/Views/Converters.cs
@@ -84,18 +84,6 @@ public sealed class InverseBoolToVisibilityConverter : IValueConverter
     }
 }
 
-public class BoolToRefreshTextConverter : IValueConverter
-{
-    public string BusyText { get; set; } = "刷新中...";
-    public string IdleText { get; set; } = "刷新节点目录";
-
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        => (value is bool b && b) ? BusyText : IdleText;
-
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        => throw new NotSupportedException();
-}
-
 public class InverseBoolConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
